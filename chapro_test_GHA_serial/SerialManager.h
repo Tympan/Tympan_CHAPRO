9f318e143ee9214e678564f7c1a310325441aa73
Chip Audette <chipaudette@yahoo.com>
2021-11-08 13:37:56 -0500

    chapro_test_GHA_serial: first commit.  Works!

---
+++ chapro_test_GHA_serial/SerialManager.h
@@ -0,0 +1,305 @@
+
+#ifndef _SerialManager_h
+#define _SerialManager_h
+
+#include <Tympan_Library.h>
+#include "AudioEffectBTNRH.h"
+#include "State.h"
+
+
+//classes from the main sketch that might be used here
+extern Tympan myTympan;                    //created in the main *.ino file
+extern State myState;                      //created in the main *.ino file
+//extern EarpieceMixer_F32_UI earpieceMixer; //created in the main *.ino file
+//extern AudioSDWriter_F32_UI audioSDWriter;
+extern AudioEffectBTNRH_F32 BTNRH_alg1; //, BTNRH_alg2;
+extern AudioEffectGain_F32 gain1;
+extern float setDigitalGain_dB(float);
+
+//
+// The purpose of this class is to be a central place to handle all of the interactions
+// to and from the SerialMonitor or TympanRemote App.  Therefore, this class does things like:
+//
+//    * Define what buttons and whatnot are in the TympanRemote App GUI
+//    * Define what commands that your system will respond to, whether from the SerialMonitor or from the GUI
+//    * Send updates to the GUI based on the changing state of the system
+//
+// You could achieve all of these goals without creating a dedicated class.  But, it 
+// is good practice to try to encapsulate some of these functions so that, when the
+// rest of your code calls functions related to the serial communciation (USB or App),
+// you have a better idea of where to look for the code and what other code it relates to.
+//
+
+//now, define the Serial Manager class
+class SerialManager : public SerialManagerBase  {  // see Tympan_Library for SerialManagerBase for more functions!
+  public:
+    SerialManager(BLE *_ble) : SerialManagerBase(_ble) {};
+
+    void printFeedbackCoeff(AudioEffectBTNRH_F32 &alg);
+      
+    void printHelp(void);
+    void createTympanRemoteLayout(void); 
+    void printTympanRemoteLayout(void); 
+    bool processCharacter(char c);  //this is called automatically by SerialManagerBase.respondToByte(char c)
+
+    //method for updating the GUI on the App
+    void setFullGUIState(bool activeButtonsOnly = false);
+    void updateGainDisplay(void);
+    void updateCpuDisplayOnOff(void);
+    void updateCpuDisplayUsage(void);
+    
+
+  private:
+
+    TympanRemoteFormatter myGUI;  //Creates the GUI-writing class for interacting with TympanRemote App
+   
+};
+
+
+void SerialManager::printHelp(void) {  
+  Serial.println("SerialManager Help: Available Commands:");
+  Serial.println(" h: Print this help");
+  Serial.println(" Static Info: ");
+  //Serial.println("      : BTNRH_alg1 setup complete = " + String(BTNRH_alg1.setup_complete) + ", BTNRH_alg2 setup complete = " + String(BTNRH_alg2.setup_complete));
+  Serial.println("  : BTNRH_alg1 setup complete = " + String(BTNRH_alg1.setup_complete));
+  Serial.println(" Gain: (no prefix)");
+  Serial.println("   k/K: incr/decrease gain (current: " + String(gain1.getGain_dB(),1) + " dB)");
+  Serial.println("   z/Z: mute/unmute");
+  Serial.println(" AFC: (no prefix)");
+  Serial.println("   a/A: incr/decrease afl, model length (current: " + String(BTNRH_alg1.get_cha_ivar(_afl)) + ")");
+  Serial.println("   w/W: incr/decrease wfl, whiten filter length (current: " + String(BTNRH_alg1.get_cha_ivar(_wfl)) + ")");
+  Serial.println("   m/M: incr/decrease mu, speed of adaptation, bigger is faster (current: " + String((float)(BTNRH_alg1.get_cha_dvar(_mu)),8) + ")");
+  Serial.println("   r/R: incr/decrease rho, smoothing (current: " + String((float)(BTNRH_alg1.get_cha_dvar(_rho)),8) + ")");
+  Serial.println("   e/E: incr/decrease eps (current: " + String((float)(BTNRH_alg1.get_cha_dvar(_eps)),8) + ")");
+  Serial.println("   s/S: print left or right AFC settings.");
+  Serial.println("   q/Q: reset left or right feedback model.");
+  Serial.println("   f/F: print left or right feedback model.  Prints ONCE.");
+  Serial.println("   d/D: start/stop REPEATED printing of LEFT feedback model.");   
+  //Serial.println("   g/G: start/stop REPEATED printing of RIGHT feedback model.");
+
+  //Add in the printHelp() that is built-into the other UI-enabled system components.
+  //The function call below loops through all of the UI-enabled classes that were
+  //attached to the serialManager in the setupSerialManager() function used back
+  //in the main *.ino file.
+  SerialManagerBase::printHelp();  ////in here, it automatically loops over the different UI elements issuing printHelp()
+  
+  Serial.println();
+}
+
+//switch yard to determine the desired action...this method is much shorter now that we're using a lot
+//of logic that has already been built-into the UI-enabled classes.
+bool SerialManager::processCharacter(char c) {  //this is called by SerialManagerBase.respondToByte(char c)
+  bool ret_val = true;
+  int ind;
+  double old_val, new_val, scale_fac=1.0;
+  
+  switch (c) {
+    case 'h':
+      printHelp(); 
+      break;
+    case 'k':
+      new_val = myState.digital_gain_dB + 3.0;
+      myTympan.println("Command received: changing gain to " + String(new_val,1) + " dB");;
+      setDigitalGain_dB(new_val);
+      break;
+    case 'K':
+      new_val = myState.digital_gain_dB - 3.0;
+      myTympan.println("Command received: changing gain to " + String(new_val,1) + " dB");;
+      setDigitalGain_dB(new_val);
+      break;
+    case 'z':
+      new_val = -200.0f;
+      myTympan.println("Command received: muting (changing gain to " + String(new_val,1) + " dB)");;
+      gain1.setGain_dB(new_val);
+      break;
+    case 'Z':
+      new_val = myState.digital_gain_dB;
+      myTympan.println("Command received: unmuting (changing gain to " + String(new_val,1) + " dB)");;
+      setDigitalGain_dB(new_val);
+      break;      
+    case 'a':
+      ind = _afl; scale_fac = 5.0;
+      old_val = BTNRH_alg1.get_cha_ivar(ind); new_val = max(0,min(150,old_val+scale_fac));
+      BTNRH_alg1.set_cha_ivar(ind, new_val);
+      BTNRH_alg1.set_cha_ivar(_in1, 0); //BTNRH_alg2.set_cha_ivar(_in1, 0);  //command afc_process to re-initialize its parameters
+      BTNRH_alg1.reset_feedback_model();
+      myTympan.print("Command received: changing AFC afl to "); myTympan.println(BTNRH_alg1.get_cha_ivar(ind));
+      break;            
+    case 'A':
+      ind = _afl; scale_fac = -5.0;
+      old_val = BTNRH_alg1.get_cha_ivar(ind); new_val = max(0,min(150,old_val+scale_fac));
+      BTNRH_alg1.set_cha_ivar(ind, new_val);
+      BTNRH_alg1.set_cha_ivar(_in1, 0); //BTNRH_alg2.set_cha_ivar(_in1, 0);  //command afc_process to re-initialize its parameters
+      BTNRH_alg1.reset_feedback_model();
+      myTympan.print("Command received: changing AFC afl to "); myTympan.println(BTNRH_alg1.get_cha_ivar(ind));
+      break;   
+    case 'w':
+      ind = _wfl; scale_fac = 1.0;
+      old_val = BTNRH_alg1.get_cha_ivar(ind); new_val = max(0,min(20,old_val+scale_fac));
+      BTNRH_alg1.set_cha_ivar(ind, new_val);
+      BTNRH_alg1.set_cha_ivar(_in1, 0); //BTNRH_alg2.set_cha_ivar(_in1, 0);  //command afc_process to re-initialize its parameters
+      BTNRH_alg1.reset_feedback_model();
+      myTympan.print("Command received: changing AFC wfl to "); myTympan.println(BTNRH_alg1.get_cha_ivar(ind));
+      break;            
+    case 'W':
+      ind = _wfl; scale_fac = -1.0;
+      old_val = BTNRH_alg1.get_cha_ivar(ind); new_val = max(0,min(20,old_val+scale_fac));
+      BTNRH_alg1.set_cha_ivar(ind, new_val);
+      BTNRH_alg1.set_cha_ivar(_in1, 0); //BTNRH_alg2.set_cha_ivar(_in1, 0);  //command afc_process to re-initialize its parameters
+      BTNRH_alg1.reset_feedback_model();
+      myTympan.print("Command received: changing AFC wfl to "); myTympan.println(BTNRH_alg1.get_cha_ivar(ind));
+      break;                 
+    case 'm':
+      ind = _mu; scale_fac = 2.0f;
+      old_val = BTNRH_alg1.get_cha_dvar(ind); new_val = old_val *= scale_fac;
+      BTNRH_alg1.set_cha_dvar(ind, new_val);  //BTNRH_alg2.set_cha_dvar(ind, new_val);
+      BTNRH_alg1.set_cha_ivar(_in1, 0); //BTNRH_alg2.set_cha_ivar(_in1, 0);  //command afc_process to re-initialize its parameters
+      myTympan.print("Command received: changing AFC mu to "); myTympan.println(BTNRH_alg1.get_cha_dvar(ind),7);
+      break;
+    case 'M':
+      ind = _mu; scale_fac = 1.0/2.0f;
+      old_val = BTNRH_alg1.get_cha_dvar(ind); new_val = old_val *= scale_fac;
+      BTNRH_alg1.set_cha_dvar(ind, new_val); //BTNRH_alg2.set_cha_dvar(ind, new_val);
+      BTNRH_alg1.set_cha_ivar(_in1, 0); //BTNRH_alg2.set_cha_ivar(_in1, 0);  //command afc_process to re-initialize its parameters
+      myTympan.print("Command received: changing AFC mu to "); myTympan.println(BTNRH_alg1.get_cha_dvar(ind),7);
+      break;
+    case 'r':
+      ind = _rho; old_val = BTNRH_alg1.get_cha_dvar(ind); new_val = 1.0-((1.0-old_val)/sqrtf(2.0));
+      BTNRH_alg1.set_cha_dvar(ind, new_val);  //BTNRH_alg2.set_cha_dvar(ind, new_val);
+      BTNRH_alg1.set_cha_ivar(_in1, 0); //BTNRH_alg2.set_cha_ivar(_in1, 0);  //command afc_process to re-initialize its parameters
+      myTympan.print("Command received: changing AFC rho to "); myTympan.println(BTNRH_alg1.get_cha_dvar(ind),7);
+      break;
+    case 'R':
+      ind = _rho; old_val = BTNRH_alg1.get_cha_dvar(ind); new_val = 1.0-((1.0-old_val)*sqrtf(2.0));
+      BTNRH_alg1.set_cha_dvar(ind, new_val);  //BTNRH_alg2.set_cha_dvar(ind, new_val);
+      BTNRH_alg1.set_cha_ivar(_in1, 0); //BTNRH_alg2.set_cha_ivar(_in1, 0);  //command afc_process to re-initialize its parameters
+      myTympan.print("Command received: changing AFC rho to "); myTympan.println(BTNRH_alg1.get_cha_dvar(ind),7);
+      break;
+    case 'e':
+      ind = _eps; scale_fac = sqrtf(10.0f); 
+      old_val = BTNRH_alg1.get_cha_dvar(ind); new_val = old_val * scale_fac;
+      BTNRH_alg1.set_cha_dvar(ind, new_val); // BTNRH_alg2.set_cha_dvar(ind, new_val);
+      BTNRH_alg1.set_cha_ivar(_in1, 0); //BTNRH_alg2.set_cha_ivar(_in1, 0);  //command afc_process to re-initialize its parameters
+      myTympan.print("Command received: changing AFC eps to "); myTympan.println(BTNRH_alg1.get_cha_dvar(ind),7);
+      break;
+    case 'E':
+      ind = _eps; scale_fac = 1.0/sqrtf(10.0f);
+      old_val = BTNRH_alg1.get_cha_dvar(ind); new_val = old_val * scale_fac;
+      BTNRH_alg1.set_cha_dvar(ind, new_val);  //BTNRH_alg2.set_cha_dvar(ind, new_val);
+      BTNRH_alg1.set_cha_ivar(_in1, 0); //BTNRH_alg2.set_cha_ivar(_in1, 0);  //command afc_process to re-initialize its parameters
+      myTympan.print("Command received: changing AFC eps to "); myTympan.println(BTNRH_alg1.get_cha_dvar(ind),7);
+      break;
+    case 'q':
+      Serial.println("SerialManager: command received...reseting LEFT AFC feedback model...");
+      for (int i=0; i<40; i++) {  BTNRH_alg1.reset_feedback_model(); delay(5); } //reset multiple times     
+      break;
+    case 'Q':
+      //Serial.println("SerialManager: command received...reseting RIGHT AFC feedback model...");
+      //BTNRH_alg2.reset_feedback_model();
+      break;    
+    case 's':
+      Serial.println("SerialManager: command received...print settings for LEFT AFC:");
+      BTNRH_alg1.print_afc_params();
+      break;
+    case 'S':
+      //Serial.println("SerialManager: command received...print settings for RIGHT AFC:");
+      //BTNRH_alg2.print_afc_params();
+      break;
+    case 'f':
+      Serial.println("SerialManager: command received...feedback model for LEFT channel:");
+      printFeedbackCoeff(BTNRH_alg1);
+      break;
+    case 'F':
+      //Serial.println("SerialManager: command received...feedback model for RIGHT channel:");
+      //printFeedbackCoeff(BTNRH_alg2);
+      break;
+    case 'd':
+      Serial.println("SerialManager: START printing feedback model for LEFT channel...");
+      myState.flag_printLeftFeedbackModel = true;
+      break;
+    case 'D':
+      Serial.println("SerialManager: STOP printing feedback model for LEFT channel...");
+      myState.flag_printLeftFeedbackModel = false;
+      break;
+    case 'g':
+      //Serial.println("SerialManager: START printing feedback model for LEFT channel...");
+      //myState.flag_printRightFeedbackModel = true;
+      break;
+    case 'G':
+      //Serial.println("SerialManager: STOP printing feedback model for LEFT channel...");
+      //myState.flag_printRightFeedbackModel = false;
+      break;
+    case 'J': case 'j':           //The TympanRemote app sends a 'J' to the Tympan when it connects
+      printTympanRemoteLayout();  //in resonse, the Tympan sends the definition of the GUI that we'd like
+      break;
+    default:
+      //if the command didn't match any of the commands, it loops through the processCharacter() methods
+      //of any UI-enabled classes that were attached to the serialManager via the setupSerialManager() 
+      //function used back in the main *.ino file.
+      ret_val = SerialManagerBase::processCharacter(c);  //in here, it automatically loops over the different UI elements
+      break; 
+  }
+  return ret_val;
+}
+
+void SerialManager::printFeedbackCoeff(AudioEffectBTNRH_F32 &alg) {
+  int n_coeff = alg.get_cha_ivar(_afl);
+  float *efbp = (float *)(alg.cp[_efbp]);
+  for (int i=0; i<n_coeff; i++) { 
+    Serial.print(efbp[i],6); 
+    if (i < n_coeff-1) Serial.print(", ");
+  }
+  Serial.println();
+}
+
+// //////////////////////////////////  Methods for defining the GUI and transmitting it to the App
+
+//define the GUI for the App
+void SerialManager::createTympanRemoteLayout(void) {
+  
+  // Create some temporary variables
+  TR_Page *page_h;  //dummy handle for a page
+  TR_Card *card_h;  //dummy handle for a card
+
+  //Add first page to GUI  (the indentation doesn't matter; it is only to help us see it better)
+  //page_h = myGUI.addPage("Earpiece Mixer");
+  //    //select inputs
+  //    card_h = earpieceMixer.addCard_audioSource(page_h); //use its predefined group of buttons for input audio source
+      
+  //Add second page for more control of earpieces
+  //page_h = earpieceMixer.addPage_digitalEarpieces(&myGUI); //use its predefined page for controlling the digital earpieces
+
+  //Add a page for some other miscellaneous stuff
+  page_h = myGUI.addPage("Globals");
+
+    //Add a button group ("card") for the CPU reporting...use a button group that is built into myState for you!
+    card_h = myState.addCard_cpuReporting(page_h);
+
+    //Add a button group for SD recording...use a button set that is built into AudioSDWriter_F32_UI for you!
+   // card_h = audioSDWriter.addCard_sdRecord(page_h);
+
+  //add some pre-defined pages to the GUI (pages that are built-into the App)
+  myGUI.addPredefinedPage("serialMonitor");
+}
+
+
+// Print the layout for the Tympan Remote app, in a JSON-ish string
+void SerialManager::printTympanRemoteLayout(void) {
+    if (myGUI.get_nPages() < 1) createTympanRemoteLayout();  //create the GUI, if it hasn't already been created
+    String s = myGUI.asString();
+    Serial.println(s);
+    ble->sendMessage(s); //ble is held by SerialManagerBase
+    setFullGUIState();
+}
+
+// //////////////////////////////////  Methods for updating the display on the GUI
+
+void SerialManager::setFullGUIState(bool activeButtonsOnly) {  //the "activeButtonsOnly" isn't used here, so don't worry about it
+
+  //Let's have the system automatically update all of the individual UI elements that we attached
+  //to the serialManager via the setupSerialManager() function used back in the main *.ino file.
+  SerialManagerBase::setFullGUIState(activeButtonsOnly); //in here, it automatically loops over the different UI elements
+
+}
+
+#endif
---
+++ chapro_test_GHA_serial/chapro_test_GHA_serial.ino
@@ -0,0 +1,172 @@
+/*
+   chapro_test_GHA
+
+   Created: Chip Audette, OpenAudio, August 2021
+
+   Purpose: Demonstrate a port of tst_gha.c from ChaPro to Tympan.
+
+   MIT License.  use at your own risk.
+*/
+
+
+//Include Arduino/Tympan related libraries
+#include <Arduino.h>
+#include <Tympan_Library.h>
+#include "implementStdio.h"   //alows for printf
+
+//Include algorithm-specific files
+#include "test_gha.h"          //see the tab "test_gha.h"..........be sure to update the name if you change the filename!
+#include "AudioEffectBTNRH.h"  //see the tab "AudioEffectBTNRH.h"
+ 
+// ///////////////////////////////////////// setup the audio processing classes and connections
+
+//set the sample rate and block size
+const float sample_rate_Hz = (int)srate;  //Set in test_gha.h.  Must be one of the values in the table in AudioOutputI2S_F32.h
+const int audio_block_samples = chunk;    //Set in test_gha.h.  Must be less than or equal to 128 
+AudioSettings_F32 audio_settings(sample_rate_Hz, audio_block_samples);
+
+// Create the Tympan
+Tympan myTympan(TympanRev::E, audio_settings);
+
+//create audio objects
+AudioInputI2S_F32     audio_in(audio_settings);
+AudioEffectBTNRH_F32  BTNRH_alg1(audio_settings);  //see tab "AudioEffectBTNRH.h"
+AudioEffectGain_F32   gain1(audio_settings);
+AudioOutputI2S_F32    audio_out(audio_settings);
+
+//connect the inputs to the BTNRH alg
+AudioConnection_F32   patchCord1( audio_in,   0, BTNRH_alg1, 0);  //left input
+AudioConnection_F32   patchCord2( BTNRH_alg1, 0, gain1, 0);
+
+//connect the BTNRH alg to the outputs
+AudioConnection_F32   patchCord11(gain1, 0, audio_out,  0);  //left output
+AudioConnection_F32   patchCord12(gain1, 0, audio_out,  1);  //right output, same as left
+
+
+// Create classes for controlling the system
+#include      "SerialManager.h"
+#include      "State.h"                            
+BLE_UI        ble(&myTympan);                                      //create bluetooth BLE
+SerialManager serialManager(&ble);                                 //create the serial manager for real-time control (via USB or App)
+State         myState(&audio_settings, &myTympan, &serialManager); //keeping one's state is useful for the App's GUI
+
+//set up the serial manager
+void setupSerialManager(void) {
+  //register all the UI elements here  
+  serialManager.add_UI_element(&myState);
+  //serialManager.add_UI_element(&ble);
+  //serialManager.add_UI_element(&earpieceMixer); 
+  //serialManager.add_UI_element(&stereoContainerWDRC);
+  //serialManager.add_UI_element(&presetManager);
+  //serialManager.add_UI_element(&audioSDWriter);
+}
+
+float setDigitalGain_dB(float val_dB) {
+    return myState.digital_gain_dB = gain1.setGain_dB(val_dB);
+}
+
+// /////////////////////////  Start the Arduino-standard functions: setup() and loop()
+
+void setup() { //this runs once at startup  
+  Serial1.begin(9600); //for talking to the Bluetooth unit (9600 is the right choice for for RevE)
+  while ((!Serial) && (millis() < 1000));  //stall for a bit to let the USB get up and running.
+  //myTympan.beginBothSerial(); delay(500);
+      
+  if (Serial) Serial.print(CrashReport);  //if it crashes and restarts, this will give some info
+  myTympan.println("CHAPRO for Tympan: Test GHA: setup():...");
+  myTympan.println("  Sample Rate (Hz): " + String(audio_settings.sample_rate_Hz));
+  myTympan.println("  Audio Block Size (samples): " + String(audio_settings.audio_block_samples));
+
+  // Audio connections require memory to work.
+  AudioMemory_F32(50, audio_settings);
+
+  // /////////////////////////////////////////////  do any setup of the algorithms
+
+  BTNRH_alg1.setup();           //in AudioEffectBTNRH.h
+  BTNRH_alg1.setEnabled(true);  //see AudioEffectBTNRH.h.  This could be done later in setup()
+ 
+  // //////////////////////////////////////////// End setup of the algorithms
+
+
+  // Start the Tympan
+  Serial.println("setup: Tympan enable...");
+  myTympan.enable();
+
+  //setup DC-blocking highpass filter running in the ADC hardware itself
+  float cutoff_Hz = 200.0;  //set the default cutoff frequency for the highpass filter
+  myTympan.setHPFonADC(true,cutoff_Hz,audio_settings.sample_rate_Hz); //set to false to disble
+
+  //Choose the desired input
+  myTympan.inputSelect(TYMPAN_INPUT_ON_BOARD_MIC);     // use the on board microphones
+  //myTympan.inputSelect(TYMPAN_INPUT_JACK_AS_MIC);    // use the microphone jack - defaults to mic bias 2.5V
+  //myTympan.inputSelect(TYMPAN_INPUT_JACK_AS_LINEIN); // use the microphone jack - defaults to mic bias OFF
+  
+  //Set the desired volume levels
+  myTympan.volume_dB(myState.output_gain_dB);                   // headphone amplifier.  -63.6 to +24 dB in 0.5dB steps.
+  float input_gain_dB = 15.0;
+  myTympan.setInputGain_dB(input_gain_dB); // set input volume, 0-47.5dB in 0.5dB setps
+
+ //setup the serial manager
+  setupSerialManager();
+
+  //set the default gain
+  Serial.println("setup(): setting overall gain to " + String(myState.digital_gain_dB));
+  setDigitalGain_dB(myState.digital_gain_dB);
+
+    //finish setup
+  Serial.println("Setup complete.");
+  serialManager.printHelp();
+}
+
+void loop() {  //this runs forever in a loop
+
+  //respond to Serial commands
+  while (Serial.available()) serialManager.respondToByte((char)Serial.read());   //USB...respondToByte is in SerialManagerBase...it then calls SerialManager.processCharacter(c)
+
+  //respond to BLE
+//  if (ble.available() > 0) {
+//    String msgFromBle; int msgLen = ble.recvBLE(&msgFromBle);
+//    for (int i=0; i < msgLen; i++) serialManager.respondToByte(msgFromBle[i]);  //respondToByte is in SerialManagerBase...it then calls SerialManager.processCharacter(c)
+//  }
+  
+  //service the LEDs...blink slow normally, blink fast if recording
+  myTympan.serviceLEDs(millis()); 
+
+  //periodically print the CPU and Memory Usage
+  if (myState.flag_printCPUandMemory) myState.printCPUandMemory(millis(), 3000); //print every 3000msec  (method is built into TympanStateBase.h, which myState inherits from)
+  if (myState.flag_printCPUandMemory) myState.printCPUtoGUI(millis(), 3000);     //send to App every 3000msec (method is built into TympanStateBase.h, which myState inherits from)
+
+  if (myState.flag_printLeftFeedbackModel) servicePrintingFeedbackModel(millis(),1000, BTNRH_alg1);
+  //if (myState.flag_printRightFeedbackModel) servicePrintingFeedbackModel(millis(),1000, BTNRH_alg2);
+}
+
+// ////////////////////////////////////////////////////////////// Servicing routines
+
+void servicePrintingFeedbackModel(unsigned long curTime_millis, unsigned long updatePeriod_millis, AudioEffectBTNRH_F32 &alg) {
+  static unsigned long lastUpdate_millis = 0;
+  //has enough time passed to update everything?
+  if (curTime_millis < lastUpdate_millis) lastUpdate_millis = 0; //handle wrap-around of the clock
+  if ((curTime_millis - lastUpdate_millis) >= updatePeriod_millis) { //is it time to update the user interface?
+
+    int n_coeff = alg.get_cha_ivar(_afl);
+    if (n_coeff > 0) {
+      //Serial.println("servicePrintingFeedbackModel: printing feedback model for AFC...");
+      float scale_fac = 100.0;  //choose whatever to make the plot prettier
+      Serial.println("Model_" + String((int)(scale_fac + 0.5)) + ":");
+      Serial.println(scale_fac);
+      Serial.println(-scale_fac);
+      Serial.println(0.0);      
+      float *efbp = (float *)(alg.cp[_efbp]);
+      for (int i=0; i<n_coeff; i++) { 
+
+        Serial.println(scale_fac*efbp[i],3); //print x decimal places
+        //if (i < n_coeff-1) Serial.print(", ");
+      }
+      //Serial.println();
+    } else {
+      Serial.println("servicePrintingFeedbackModel: printing feedback model for AFC...AFC is disabled.");
+    }
+
+    lastUpdate_millis = curTime_millis; //we will use this value the next time around.
+  }
+}
